c574f12125de160c83f708da17689d5931dc6df1
jacobfrisbie <jacobfrisbie@outlook.com>
2022-11-18 16:16:07 -0600

    Final Commit
    Final commit of lab with completed logic and added type hinting + docstrings

--- account.py
+++ account.py
@@ -1,27 +1,27 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name):  # Create objects inside Account class
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount):  # Deposit method adds to the account balance
         if amount > 0:
-            self.__account_balance = self.__account_balance + amount
+            self.__account_balance = self.__account_balance + amount  # Add amount deposited to account balance
             return True
         else:
             return False
 
-    def withdraw(self, amount):
-        if amount > 0:
-            if amount <= self.__account_balance:
-                self.__account_balance = self.__account_balance - amount
+    def withdraw(self, amount):  # Withdraw method withdraws from the account balance
+        if amount > 0:  # Check the account for a positive balance
+            if amount <= self.__account_balance:  # Limiting withdraws to less or equal to account balance
+                self.__account_balance = self.__account_balance - amount  # Subtracting amount deposited from balance
                 return True
             else:
                 return False
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self):  # Get balance method to retrieve account balance private variable
         return self.__account_balance
 
-    def get_name(self):
-        return self.__account_name #test
+    def get_name(self):  # Get name method to retrieve account name private variable
+        return self.__account_name
--- test_account.py
+++ test_account.py
@@ -1,10 +1,21 @@
 import pytest
-from account import account
+from account import Account  # Import the Account class from account.py
 
-def test_init():
-    account1 = account("Jacob")
-    assert account1.getname() == "Jacob"
-    assert account1.getbalance() == 0
 
-def test_deposit():
-    
+def test_init():  # Test init method
+    testAccount = Account("Jacob")  # Create Account with name "Jacob"
+    assert testAccount.get_name() == "Jacob"  # Check the name is Jacob
+    assert testAccount.get_balance() == 0  # Check the balance is zero
+
+
+def test_deposit():  # Test deposit method
+    testAccount = Account("Jacob")  # Create the Account
+    testAccount.deposit(35)  # Deposit 35 using the deposit method
+    assert testAccount.get_balance() == 35  # Check the balance
+
+
+def test_withdraw():  # Test withdraw method
+    testAccount = Account("Jacob")  # Create the Account
+    testAccount.deposit(35)
+    testAccount.withdraw(15)
+    assert testAccount.get_balance() == 20  # Check the balance
